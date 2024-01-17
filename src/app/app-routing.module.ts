e26228677e688e81f3bca45b09b5268f1d6998c1
hejunji <junji.he@biomind.ai>
2024-01-17 17:03:17 +0800

    chore: 增加文件上传

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BaseToPngComponent} from './app/components/base-to-png/base-to-png.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: BaseToPngComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
+import { BaseToPngComponent } from './app/components/base-to-png/base-to-png.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseToPngComponent
   ],
     imports: [
         BrowserModule,
