f1bbfac04c1a9d4b1d011bc73dc4a08f2d0f59da
hejunji <junji.he@biomind.ai>
2024-01-17 15:38:25 +0800

    chore: 代码优化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,15 @@ export class AppComponent implements AfterViewInit {
     baseString: '',
     imgType: '',  // 图像类型
     backgrounds: [
-      {color: '#000', label: "黑"},
-      {color: '#fff', label: "白"},
-      {color: '#f00', label: "红"},
+      {color: '#000', label: '黑'},
+      {color: '#fff', label: '白'},
+      {color: '#f00', label: '红'},
     ],
     background: '',
   };
   imgTypes = [
-    {type: 'png', name: "PNG"},
-    {type: 'Uint8', name: "Uint8"}
+    {type: 'png', name: 'PNG'},
+    {type: 'Uint8', name: 'Uint8'}
   ];
 
   baseString: string;
@@ -42,14 +42,14 @@ export class AppComponent implements AfterViewInit {
     }, 0);
   }
 
-  pngVal() {
+  pngVal(): void {
     // console.log("origin: ",this.pngConfig.baseString);
-    let str = this.pngConfig.baseString;
+    const str = this.pngConfig.baseString;
     if (!str) {
       this.baseString = null;
-      return
+      return;
     }
-    this.baseString = str.trim().replace("data: ", '').replace(/^\"|\"$|^\'|\'$/g, '').trim();
+    this.baseString = str.trim().replace(/^"|"$|^'|'$/g, '').replace('data: ', '').trim();
     this.decode();
   }
 
@@ -60,47 +60,47 @@ export class AppComponent implements AfterViewInit {
     let data = 'data:image/png;base64,';
     if (this.pngConfig.baseString.includes('base64,')) {
       data = this.pngConfig.baseString;
-    } else if (this.pngConfig.imgType === "png") {
-      data += this.baseString.replace(/^\"|\"$|^\'|\'$/g, '').trim();
+    } else if (this.pngConfig.imgType === 'png') {
+      data += this.baseString.replace(/^"|"$|^'|'$/g, '').trim();
     } else {
       let png: any;
-      let array;
+      let array: Uint8Array;
       try {
         array = this._base64ToUint8Array(this.baseString);
       } catch (e) {
-        console.log("decode base64 error, ", e);
+        console.log('decode base64 error, ', e);
         return;
       }
 
-      let width;
-      let height;
-      let pixelDataOriginal;
+      let width: any;
+      let height: any;
+      let pixelDataOriginal: number[] | Uint8Array;
       // https://www.cnblogs.com/zhangnan35/p/12433201.html
-      if (this.pngConfig.imgType === "Uint8") {
+      if (this.pngConfig.imgType === 'Uint8') {
 
         try {
           png = PNG.newPng(array);
         } catch (e) {
-          console.log("decode png error, ", e);
+          console.log('decode png error, ', e);
           return;
         }
         width = png.width;
         height = png.height;
         // console.log("array", png, width, height);
-        let decodePixels = png.decodePixels();
+        const decodePixels = png.decodePixels();
         let buffData;
         let currentColor;
-        let colorDml = [255, 0, 0];
-        let colorDisease = [255, 255, 0];
+        const colorDml = [255, 0, 0];
+        const colorDisease = [255, 255, 0];
         switch (png.pixelBitlength) {
           case 8:
-            //grayscale
+            // grayscale
             pixelDataOriginal = new Uint8Array(png.width * png.height * 4);
             buffData = new Uint8Array(decodePixels.buffer);
 
             for (let i = 0; i < png.width * png.height; i++) {
               if (buffData[i] > 0) {
-                //lable2动脉瘤(红色),lable1血管(绿色)
+                // lable2动脉瘤(红色),lable1血管(绿色)
                 if (buffData[i] === 2) {
                   currentColor = [...colorDml];
                 } else {
@@ -117,53 +117,16 @@ export class AppComponent implements AfterViewInit {
             break;
         }
       } else {
-        // int16array
-        // let pixelData;
-        // try {
-        //   let ab8 = this._base64ToUint8Array(this.baseString);
-        //   pixelData = new Int16Array(ab8.buffer);
-        //   // let base64 = this.buffer2Base64(ab);
-        //   // console.log(base64);
-        //   // const img = new ImageData(
-        //   //   new Uint8ClampedArray(pixelData.buffer),
-        //   //   389,
-        //   //   217
-        //   // );
-        //   // return `data:image/png;base64,${base64}`;
-        // } catch (e) {
-        //   console.error("decode base64 error, ", e);
-        //   return;
-        // }
-        // width = 512;
-        // height = 672;
-        // let {minPixelValue, maxPixelValue} = this.getPixelValues(pixelData);
-        // pixelDataOriginal = pixelData.buffer;
-        // let canvasImageDataData = new ImageData(width, height);
-        // let numPixels = pixelData.length;
-        // let canvasImageDataIndex = 3;
-        // let storedPixelDataIndex = 0;
-        // if (pixelData instanceof Int16Array) {
-        //   if (minPixelValue < 0) {
-        //     while (storedPixelDataIndex < numPixels) {
-        //       canvasImageDataData[canvasImageDataIndex] = pixelData[storedPixelDataIndex++] + -minPixelValue; // Alpha
-        //       canvasImageDataIndex += 4;
-        //     }
-        //   } else {
-        //     while (storedPixelDataIndex < numPixels) {
-        //       canvasImageDataData[canvasImageDataIndex] = pixelData[storedPixelDataIndex++]; // Alpha
-        //       canvasImageDataIndex += 4;
-        //     }
-        //   }
-        // }
       }
-      data = this._renderPixel({pixelData: pixelDataOriginal, width, height})
+      data = this._renderPixel({pixelData: pixelDataOriginal, width, height});
     }
     return data;
   }
+
   getPixelValues(pixelData): any {
     let minPixelValue = Number.MAX_VALUE;
     let maxPixelValue = Number.MIN_VALUE;
-    let len = pixelData.length;
+    const len = pixelData.length;
     let pixel = void 0;
 
     for (let i = 0; i < len; i++) {
@@ -173,87 +136,83 @@ export class AppComponent implements AfterViewInit {
     }
 
     return {
-      minPixelValue: minPixelValue,
-      maxPixelValue: maxPixelValue
+      minPixelValue,
+      maxPixelValue
     };
-  };
-
-  str2ab(str) {
-    let buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-    let bufView = new Array(buf);
-    for (let i = 0, strLen = str.length; i < strLen; i++) {
-      bufView[i] = str.charCodeAt(i);
-    }
-    return bufView;
   }
 
-  buffer2Base64(int16) {
-    // const binary = String.fromCharCode.apply(null, buffer);
-    // return window.btoa(binary);
-    return window.btoa(int16.reduce(
-      function (data, byte) {
-        return data + String.fromCharCode(byte);
-      },
-      ''
-    ));
-  }
+  // str2ab(str) {
+  //   const buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+  //   const bufView = new Array(buf);
+  //   for (let i = 0, strLen = str.length; i < strLen; i++) {
+  //     bufView[i] = str.charCodeAt(i);
+  //   }
+  //   return bufView;
+  // }
+  //
+  // buffer2Base64(int16) {
+  //   // const binary = String.fromCharCode.apply(null, buffer);
+  //   // return window.btoa(binary);
+  //   return window.btoa(int16.reduce(
+  //     function (data, byte) {
+  //       return data + String.fromCharCode(byte);
+  //     },
+  //     ''
+  //   ));
+  // }
 
   _base64ToUint8Array(base64String: string): Uint8Array {
-    let rawData = window.atob(base64String) || "";
-    let outputArray = new Uint8Array(rawData.length);
+    const rawData = window.atob(base64String) || '';
+    const outputArray = new Uint8Array(rawData.length);
     for (let i = 0; i < rawData.length; ++i) {
       outputArray[i] = rawData.charCodeAt(i);
     }
     return outputArray;
   }
 
-  _renderPixel({pixelData, width, height}) {
-    let canvas: any = document.createElementNS('http://www.w3.org/1999/xhtml', 'canvas');
-    let ctx = canvas.getContext('2d', {alpha: false});
-    let imageData = new ImageData(new Uint8ClampedArray(pixelData), width, height);
-    ctx.putImageData(imageData, 0, 0)
-    let base64 = canvas.toDataURL();
+  _renderPixel({pixelData, width, height}): string {
+    const canvas: any = document.createElementNS('http://www.w3.org/1999/xhtml', 'canvas');
+    const ctx = canvas.getContext('2d', {alpha: false});
+    const imageData = new ImageData(new Uint8ClampedArray(pixelData), width, height);
+    ctx.putImageData(imageData, 0, 0);
+    const base64 = canvas.toDataURL();
     return base64;
   }
 
-  setSelectionRange() {
-    let input = document.querySelector('textarea');
+  setSelectionRange(): void {
+    const input = document.querySelector('textarea');
     input.select();
-    document.execCommand && document.execCommand('paste');
+    if (document.execCommand) {
+      document.execCommand('paste');
+    }
 
     setTimeout(() => {
       this.pngVal();
     }, 10);
   }
 
-  onImageLoad() {
+  onImageLoad(): void {
     // 将解析成功的图片添加到历史
     this.setBase64List(this.baseString);
   }
 
-  onImageError($event: Event) {
+  onImageError($event: Event): void {
     console.log('onImageError', $event);
   }
 
-  getBase64List() {
+  getBase64List(): Array<any> {
     return JSON.parse(localStorage.getItem(STORAGE_BASE_NAME) || '[]');
   }
 
-  setBase64List(base64) {
-    let list = this.getBase64List();
+  setBase64List(base64: string): void {
+    const list = this.getBase64List();
     if (list.find((item) => item.base64 === base64)) {
       return;
     }
     list.unshift({base64, date: new Date()});
-    list.length > 5 && (list.length = 5);
+    if (list.length > 5) {
+      list.length = 5;
+    }
     localStorage.setItem(STORAGE_BASE_NAME, JSON.stringify(list));
-  }
-
-  initData() {
-    // this.baseString = INIT_BASE64_STRING;
-    // console.log(this.baseString);
-    // this.list[0] && this.list[0].base64
-    //   ? this.list[0].base64
-    //   : INIT_BASE64_STRING;
   }
 }
